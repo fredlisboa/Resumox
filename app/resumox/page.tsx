0e0787b4a72678fb4897a0d9b021d1e493fedac7
Fred Lisboa <frederiquelisboa@gmail.com>
2026-03-05 18:06:22 -0300

    65

--- app/resumox/page.tsx
+++ app/resumox/page.tsx
@@ -192,7 +192,7 @@ export default function ResumoxLoginPage() {
                   <svg className="w-5 h-5 flex-shrink-0 drop-shadow-[0_0_6px_rgba(255,107,107,0.6)]" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                   </svg>
-                  Use o mesmo email que você cadastrou na Cakto.
+                  Use o mesmo email que você cadastrou no ato da compra.
                 </p>
               </div>
               <input
